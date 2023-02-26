667c1f4d1da939b5f551131e47c08774157e8719
Samir Karimov <samir.karimov.01@gmail.com>
2023-02-26 12:42:58 +0300

    Добавленна 10 задача ДЗ

--- HomeWorkSem2/Task10/Program.cs
+++ HomeWorkSem2/Task10/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа. Через строку решать нельзя.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Write("Введите трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num > 99 && num < 1000)
+{
+    int res = num / 10;
+    num = res % 10;
+    Console.WriteLine($"Второе число - {num}");
+}
+else Console.WriteLine($"Число {num} не является трехзначным!!!");
