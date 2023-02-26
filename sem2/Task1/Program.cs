c6c4655cdd009d9c70dd877498ded325e9f6baf3
Samir Karimov <samir.karimov.01@gmail.com>
2023-02-26 12:02:04 +0300

    Изменили структуру репозитория Э

--- sem2/Task1/Program.cs
+++ sem2/Task1/Program.cs
@@ -5,7 +5,6 @@
 // 78 -> 8
 // 12-> 2
 // 85 -> 8
-
 int num = new Random().Next(10,100);
 Console.WriteLine($"Было сгенерировано число {num}");
 int x1 = num / 10;
@@ -16,3 +15,7 @@ else if (x1==x2)
     Console.WriteLine("Все цифры этого числа равны");
 else    
     Console.WriteLine($"Наибольшая цифра это {x2}");
+
+//выведем в строку все цифры от 1 до сгенерированного
+for (int i=1; i<=num;i++)
+    Console.Write(i + " ");
---
+++ sem2/Task6/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int digitsCount = (int)Math.Log10(number) + 1; 
+int multiplier = (int)Math.Pow(10, digitsCount - 2); 
+int result = number % multiplier + number / multiplier / 10 * multiplier; // все что справа от разряда + все что слева, делённое на 10
+Console.WriteLine(result); 
