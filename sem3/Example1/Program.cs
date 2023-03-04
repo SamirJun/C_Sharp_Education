ec6a18d9ffb70b6228c87fdb838e795ff6b3f319
Samir Karimov <samir.karimov.01@gmail.com>
2023-03-04 11:02:00 +0300

    Добавили задания по 3 семинару

---
+++ sem3/Example1/Program.cs
@@ -0,0 +1,40 @@
+﻿void CheckCord(int x, int y)
+{
+    if (x > 0 && y > 0)
+        Console.WriteLine("Четверть №1");
+    else if (x > 0 && y < 0)
+        Console.WriteLine("Четверть №4");
+    else if (x < 0 && y < 0)
+        Console.WriteLine("Четверть №3");
+    else if (x < 0 && y > 0)
+        Console.WriteLine("Четверть №2");
+    else if (x == 0 | y == 0)
+        Console.WriteLine("x, y равно 0");
+}
+
+int CheckCord2(int x, int y)
+{
+    int res = 0;
+    if (x > 0 && y > 0)
+        res =1;
+    else if (x > 0 && y < 0)
+        res = 4;
+    else if (x < 0 && y < 0)
+        res = 3;
+    else if (x < 0 && y > 0)
+        res = 2;
+    
+    return res;
+}
+
+
+
+Console.WriteLine("Введите координату X ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату Y");
+int y = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Координаты находятся в четверти № {CheckCord2(x,y)}");
+
+
+
---
+++ sem3/Example2/Program.cs
@@ -0,0 +1,42 @@
+﻿void CheckCord(int xy)
+{
+   
+    if (xy == 1)
+        Console.WriteLine("Точки находятся в диапазоне x>0 & y>0");
+    else if (xy == 2)
+        Console.WriteLine("Точки находятся в диапазоне x<0 & y>0");
+    else if (xy == 3)
+        Console.WriteLine("Точки находятся в диапазоне x<0 & y<0");
+    else if (xy==4)
+        Console.WriteLine("Точки находятся в диапазоне x>0 & y<0");
+    else 
+        Console.WriteLine("Вы ввели число не в диапазоне от 1 до 4");
+}
+
+string  CheckCord2(int arg)
+{
+
+    string res ="Ошибка";
+    if (arg == 1)
+        res ="Точки находятся в диапазоне x>0 & y>0";
+    else if (arg == 2)
+        res = "Точки находятся в диапазоне x<0 & y>0";
+    else if (arg == 3)
+        res = "Точки находятся в диапазоне x<0 & y<0";
+    else if (arg == 4)
+        res = "Точки находятся в диапазоне x>0 & y<0";
+    
+    return res;
+}
+
+
+
+Console.WriteLine("Введите № четверти ");
+int xy = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите координату Y");
+// int y = Convert.ToInt32(Console.ReadLine());
+
+CheckCord(xy);
+
+Console.WriteLine("");
+Console.WriteLine(CheckCord2(xy));
