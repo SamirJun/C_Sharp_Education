7b94026b1fb17b509893d8cb9cb41ec109cebf6b
Samir Karimov <samir.karimov.01@gmail.com>
2023-03-10 13:55:04 +0300

    Добавили ДЗ 4 семинара

---
+++ HomeWorkSem4/Task25/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int Exponentiation(int arg1, int count)
+{
+    int i = 1;
+    int result = arg1;
+    while (i< count)
+    {
+        result = result * arg1;
+        i++;
+    }
+    return result;
+}
+
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите степень: ");
+int step = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(Exponentiation(number,step));
---
+++ HomeWorkSem4/Task27/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int Sum(int arg1,int count)
+{
+    int i = 1;
+    int result = 0;
+    while (i <= count)
+    {
+        result = result + arg1%10;
+        arg1 = arg1/10;
+        i++;
+    }
+    return result;
+}
+
+Console.WriteLine("Введите число: ");
+int number = Convert .ToInt32(Console.ReadLine());
+
+int count = (int)Math.Log10(number)+1;
+
+Console.WriteLine(Sum(number,count));
