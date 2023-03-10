4307531c37d9c9e4ed81c4676d7fbff609000aa7
Samir Karimov <samir.karimov.01@gmail.com>
2023-03-10 18:59:55 +0300

    Добавлены материалы по 3 лекции

---
+++ less3/Example1/Program.cs
@@ -0,0 +1,44 @@
+﻿
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 15);
+        index++;
+    }
+}
+
+void PrintArray(int[] arr)
+{
+    int count = arr.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void SelectionSort(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        int minPosition = i;
+        for (int j = i + 1; j < arr.Length; j++)
+        {
+            if (arr[j] < arr[minPosition]) minPosition = j;
+        }
+
+        int temp = arr[i];
+        arr[i] = arr[minPosition];
+        arr[minPosition] = temp;
+    }
+}
+
+
+int[] Array = new int[8];
+FillArray(Array);
+PrintArray(Array);
+SelectionSort(Array);
+PrintArray(Array);
